6d3a9f6cc0fea0e3af058ef925e9623c3b3e7a47
ShadowGraphs <henrik-wolf@freenet.de>
2022-10-12 17:25:51 +0200

    added sensible defaults to plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,30 +1,36 @@
 function Folium.draw!(fig::FoliumMap, g::T, series_type::Symbol; kwargs...) where {T<:AbstractMetaGraph}
     kw = Dict{Symbol, Any}(kwargs)
+    println(kw)
     if series_type === :vertices
+        kw[:radius] = get(kw, :radius, 2)
         for vertex in vertices(g)
             lon = get_prop(g, vertex, :lon)
             lat = get_prop(g, vertex, :lat)
             draw!(fig, lon, lat, :circle; kw...)
         end
     elseif series_type === :edges
+        kw[:opacity] = get(kw, :opacity, 0.5)
+        kw[:weight] = get(kw, :weight, 5)
+        kw[:color] = get(kw, :color, "#bbbbbb")
         for edge in edges(g)
             sla = get_prop(g, src(edge), :lat)
             slo = get_prop(g, src(edge), :lon)
             dla = get_prop(g, dst(edge), :lat)
             dlo = get_prop(g, dst(edge), :lon)
-            draw!(fig, [slo, dlo], [sla, dla], :line)
+            draw!(fig, [slo, dlo], [sla, dla], :line; kw...)
         end
     elseif series_type === :edgegeom
         for edge in edges(g)
-            !has_prop(g, edge, :geolinestring) && continue
-            linestring = get_prop(g, edge, :geolinestring)
-            draw!(fig, linestring)
+            !has_prop(g, edge, :edgegeom) && continue
+            linestring = get_prop(g, edge, :edgegeom)
+            draw!(fig, linestring; kw...)
         end
     elseif series_type === :shadowgeom
+        kw[:color] = get(kw, :color, "black")
         for edge in edges(g)
             !has_prop(g, edge, :shadowgeom) && continue
             line = get_prop(g, edge, :shadowgeom)
-            draw!(fig, line)
+            draw!(fig, line; kw...)
         end
     else
         throw(ArgumentError("the series type $series_type is not supported. Available types are: [:vertices, :edges, :edgegeom, :shadowgeom]"))
