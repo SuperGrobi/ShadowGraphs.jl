a9f71b08b0e26f6f5373a6de1ede86ccb71f7ca3
ShadowGraphs <henrik-wolf@freenet.de>
2022-11-15 11:32:24 +0100

    refactored with CoolWalkUtils

--- src/ShadowGraphs.jl
+++ src/ShadowGraphs.jl
@@ -1,4 +1,5 @@
 module ShadowGraphs
+using CoolWalksUtils
 using LightOSM
 using LightXML
 using Graphs
@@ -9,20 +10,6 @@ using ArchGDAL
 using Statistics
 using DataFrames
 using CSV
-
-const OSM_ref = Ref{ArchGDAL.ISpatialRef}()
-
-function __init__()
-    OSM_ref[] = ArchGDAL.importEPSG(4326; order=:trad)
-end
-# TODO: this is a duplicate from Composite Buildings. Something like this should idealy
-# be integrated into archGDAL itself...
-function apply_wsg_84!(geom)
-    ArchGDAL.createcoordtrans(OSM_ref[], OSM_ref[]) do trans
-        ArchGDAL.transform!(geom, trans)
-    end
-end
-
 
 export shadow_graph_from_object, shadow_graph_from_file, shadow_graph_from_download
 include("buildGraph.jl")
