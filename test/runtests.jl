2a585da2ca3899fc62929f19be91f17aa72c0459
ShadowGraphs <henrik-wolf@freenet.de>
2022-11-25 17:33:57 +0100

    removed legacy tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,41 +7,6 @@ using ArchGDAL
 using CoolWalksUtils
 using Test
 
-@testset "Circularity weirdness" begin
-    @test true
-
-    @test true
-    tags = Dict{String, Any}("oneway"=>false)
-    roundabout_start_end = Way(1, [1,2,3,4,5,6,7,8,1], tags)
-    roundabout_non_start = Way(1, [8,1,2,3,4,5,6,7,8], tags)
-    selfloop_start = Way(1, [1,2,3,4,1], tags)
-    selfloop_non_start = Way(1, [4,1,2,3,4], tags)
-    
-    for start_index in [1,2,3,4]
-        neighs = ShadowGraphs.get_neighbor_osm_ids(roundabout_start_end, start_index, [1,3,5,7])
-        println(neighs)
-    end
-    println("")
-    for start_index in [1,2,3,4]
-        neighs = ShadowGraphs.get_neighbor_osm_ids(roundabout_non_start, start_index, [1,3,5,7])
-        println(neighs)
-    end
-    println("")
-    for start_index in [1]
-        neighs = ShadowGraphs.get_neighbor_osm_ids(selfloop_start, start_index, [1])
-        println(neighs)
-    end
-    println("")
-    for start_index in [1]
-        neighs = ShadowGraphs.get_neighbor_osm_ids(selfloop_start, start_index, [1])
-        println(neighs)
-    end
-    println("")
-    lolipop = Way(1, [9,2,3,4,5,6,3], tags)
-    neighs = ShadowGraphs.get_neighbor_osm_ids(lolipop, 1, [9])
-    println(neighs)
-end
-
 include("BuildGraph.jl")
 include("Persistence.jl")
 include("Plotting.jl")
