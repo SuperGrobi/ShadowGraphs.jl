20cbc0f67edf24b8a724a4b686f6baf58d5fbf5c
ShadowGraphs <henrik-wolf@freenet.de>
2023-03-24 21:20:24 +0100

    added test skeleton

--- test/NodeSampling.jl
+++ test/NodeSampling.jl
@@ -1,4 +1,15 @@
 @testset "NodeSampling.jl" begin
+    @testset "get_points_to_consolidate" begin
+
+    end
+
+    @testset "get_closest_id" begin
+
+    end
+
+    @testset "get_inbound_lengths" begin
+
+    end
     @testset "consolidate_nodes_geom" begin
 
     end
