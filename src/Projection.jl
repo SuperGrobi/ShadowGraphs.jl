2ccd9e5c7093be843dacac8289bf2eb338c41a6e
ShadowGraphs <henrik-wolf@freenet.de>
2023-04-05 15:11:01 +0200

    added and fixed tests for projection

--- src/Projection.jl
+++ src/Projection.jl
@@ -1,18 +1,19 @@
 """
 
-    project_local!(g::T, center_lon, center_lat) where {T<:AbstractMetaGraph}
+    project_local!(g::T, center_lon=get_prop(g, :center_lon), center_lat=get_prop(g, :center_lat)) where {T<:AbstractMetaGraph}
 
 projects all `ArchGDAL` geometry in the `props` of edges and nodes of `g` to transverse mercator, centered at `center_lon`, `center_lat`
-and updates the `:crs` properity of `g` accordingly. All geometry is assumed to be in the `:crs` system.
+and updates the `:crs` properity of `g` accordingly. All geometry is assumed to be in the `:crs` system. Returns the projected `g`.
 """
-function CoolWalksUtils.project_local!(g::T, center_lon, center_lat) where {T<:AbstractMetaGraph}
+function CoolWalksUtils.project_local!(g::T, center_lon=get_prop(g, :center_lon), center_lat=get_prop(g, :center_lat)) where {T<:AbstractMetaGraph}
     projstring = "+proj=tmerc +lon_0=$center_lon +lat_0=$center_lat"
     #println(projstring)
     src = get_prop(g, :crs)
     dest = ArchGDAL.importPROJ4(projstring)
-    ArchGDAL.createcoordtrans(trans->project_graph_edges!(g, trans), src, dest)
-    ArchGDAL.createcoordtrans(trans->project_graph_nodes!(g, trans), src, dest)
+    ArchGDAL.createcoordtrans(trans -> project_graph_edges!(g, trans), src, dest)
+    ArchGDAL.createcoordtrans(trans -> project_graph_nodes!(g, trans), src, dest)
     set_prop!(g, :crs, dest)
+    return g
 end
 
 """
@@ -20,12 +21,14 @@ end
     project_back!(g::T) where {T<:AbstractMetaGraph}
 
 projects all `ArchGDAL` geometry in the `props` of edges and nodes of `g` back to `EPSG4236` and updates the `:crs` property of `g` accordingly.
+Returns `g`.
 """
 function CoolWalksUtils.project_back!(g::T) where {T<:AbstractMetaGraph}
     src = get_prop(g, :crs)
-    ArchGDAL.createcoordtrans(trans->project_graph_edges!(g, trans), src, OSM_ref[])
-    ArchGDAL.createcoordtrans(trans->project_graph_nodes!(g, trans), src, OSM_ref[])
+    ArchGDAL.createcoordtrans(trans -> project_graph_edges!(g, trans), src, OSM_ref[])
+    ArchGDAL.createcoordtrans(trans -> project_graph_nodes!(g, trans), src, OSM_ref[])
     set_prop!(g, :crs, OSM_ref[])
+    return g
 end
 
 """
